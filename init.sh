c039c4c29d551621b1e3e356dac79937646fc2c8
Harold Zhang <HZhang@datacolor.com>
2025-08-26 14:15:13 +0800

    功能： 新增初始化脚本

---
+++ init.sh
@@ -0,0 +1,11 @@
+# 切换到root用户
+sudo -i
+
+# 下载脚本
+wget -O install_singbox.sh https://raw.githubusercontent.com/HaroldZhangCode91/sing-box-installShell/refs/heads/main/install_singbox.sh
+
+# 赋予执行权限
+chmod +x install_singbox.sh
+
+# 运行脚本
+./install_singbox.sh 30707
